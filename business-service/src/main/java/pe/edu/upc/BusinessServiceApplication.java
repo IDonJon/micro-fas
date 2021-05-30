20491df3358db76dcf540b3a2d43f495e9ec9103
IDonJon <71906602+IDonJon@users.noreply.github.com>
2021-05-30 18:56:30 -0500

    ................

--- business-service/src/main/java/pe/edu/upc/BusinessServiceApplication.java
+++ business-service/src/main/java/pe/edu/upc/BusinessServiceApplication.java
@@ -2,9 +2,17 @@ package pe.edu.upc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
+@EnableHystrix
+@EnableHystrixDashboard
 public class BusinessServiceApplication {
 
     public static void main(String[] args) {
